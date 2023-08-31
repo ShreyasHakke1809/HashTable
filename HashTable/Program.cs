3719e3860745a8c5e675f918e517505322a93c4c
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-08-31 08:01:05 +0530

    Creating binary search tree program

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -7,6 +7,13 @@
             Console.WriteLine("Welcome to HashTable Program");
             string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
             FrequencyOfWords.GetFrequencyOfWords(paragraph);
+
+            Console.WriteLine("Welcome to Binary Search Tree");
+            MyBinarySearchTree<int> binaryTree = new MyBinarySearchTree<int>(56);
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Display();
+
         }
     }
 }
