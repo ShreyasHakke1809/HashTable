48fadd10a88078f8fa3647bdd933aca28f25e1be
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-08-31 07:44:02 +0530

    Adding to find frequency of words program

---
+++ HashTable/FrequencyOfWords.cs
@@ -0,0 +1,28 @@
+﻿namespace HashTable
+{
+    internal class FrequencyOfWords
+    {
+        public static void GetFrequencyOfWords(string paragraph)
+        {
+            MyMapNode<string, int> hashTable = new MyMapNode<string, int>(8);
+            string[] words = paragraph.Split(' ');
+            foreach (string word in words)
+            {
+                if (hashTable.Exists(word))
+                {
+                    hashTable.Add(word.ToLower(), hashTable.Get(word.ToLower()) + 1);
+                }
+                else
+                {
+                    hashTable.Add(word.ToLower(), 1);
+                }
+            }
+            Console.WriteLine("Displaying after Add operatopn:");
+            hashTable.Display();
+            string s = "or";
+            hashTable.Remove(s);
+            Console.WriteLine("After removing an item- {0}:", s);
+            hashTable.Display();
+        }
+    }
+}
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HashTable Program");
+            string paragraph = "To be or not to be or not";
+            FrequencyOfWords.GetFrequencyOfWords(paragraph);
         }
     }
 }
